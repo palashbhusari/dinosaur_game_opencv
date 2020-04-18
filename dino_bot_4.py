5af1309ed69583999c4ba1e348378f6659587399
palashbhusari <palashshankarbhusari@gmail.com>
2020-04-18 21:17:05 +0530

    region of interest

--- dino_bot_4.py
+++ dino_bot_4.py
@@ -16,13 +16,13 @@ lx2,ly2= 210,358
 # x from left increase to go rigth 
 # y from top increase to go down
 
-px0,py0= 215,337 # lower point of rect
+###px0,py0= 215,337 # lower point of rect
 w0,h0=10,10  # weidth and height downward 
 
 px1,py1=210,312  # upper rect
 
-px2,py2=146,337 # rect on dino on the dino
-w2,h2=60,5
+px2,py2=158,337 # rect on dino on the dino
+w2,h2=70,3
 bgx,bgy=50,330
 
 def draw():
@@ -30,7 +30,7 @@ def draw():
     cv2.circle(img, (bgx,bgy), 2, (255, 0, 0), -1)  
     # rect in front
     # square for cactus
-    cv2.rectangle(img,(px0,py0),(px0+w0,py0+h0),(0,0,255),1)   
+    ###cv2.rectangle(img,(px0,py0),(px0+w0,py0+h0),(0,0,255),1)   
     # square for bird
     cv2.rectangle(img,(px1,py1),(px1+w0,py1+h0),(0,0,255),1)
     # rect below dino
@@ -50,13 +50,13 @@ def find_value(event,x,y,flags,param):
 def roi():
     global roi0,roi1,roi2
     # # roi0 to detect obstacle
-    roi0=int(np.sum(img[py0:py0+h0,px0:px0+w0])# region o i 208,240 = x,y
+    ###roi0=int(np.sum(img[py0:py0+h0,px0:px0+w0])# region o i 208,240 = x,y
     #print((np.sum(roi0)))# = 24700
     roi1=np.sum(img[py1:py1+h0,px1:px1+w0])
 
     ##roi2 to detect obstale under dino
     roi2=int(np.sum(img[py2:py2+h2,px2:px2+w2]))# region o i 208,240 = x,y
-    #print(roi2,type(roi2)) #=
+    #print(roi2) #=51870
     
 bounding_box = {"top": 100, "left": 0, "width": 830, "height": 500}
 sct = mss()
@@ -76,23 +76,24 @@ while True:
     ##pixel values
     #px_d0= img[py0,px0]# lower front pixel
     #px_d1= img[py1,px1]# upper front pixel 
-    #px_d2= img[py2,px2] 
-    
+    px_d2= img[py2,px2] 
+    print(px_d2)
     #if (px_d1 != background) or (px_d0 != background): #[255,255,255,255] for white back ground
-    if roi0 != 24700:
+    if roi2 != 49902:
         press('space')
-        while(1):
+        while(0):
             sct_img = sct.grab(bounding_box)
             img=np.array(sct_img)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             
-            roi()
-            
-            if (roi0 == 24700) and (roi2 == 74100):
+            px_d2= img[py2,px2]
+
+            if roi2 == 51870: # dino in air
                 press('down')
+                #print('down')
                 break
 
-    elif roi1 != 24700:
+    elif roi1 != 24700: # px1,py1 upper square
         keyDown('down')
         time.sleep(0.08)
         keyUp('down')
